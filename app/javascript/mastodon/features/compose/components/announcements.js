a6699972fa42cdf9f3fe32910db000c7d90c11f8
fvh_P <ko_kurihara@yahoo.co.jp>
2017-06-14 07:18:32 +0000

    collapsible announcement

--- app/javascript/mastodon/features/compose/components/announcements.js
+++ app/javascript/mastodon/features/compose/components/announcements.js
@@ -4,7 +4,26 @@ import Immutable from 'immutable';
 import PropTypes from 'prop-types';
 import Link from 'react-router/lib/Link';
 import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
-import IconButton from '../../../components/icon_button';
+import IconButton from '../../../components/announcement_icon_button';
+import Motion from 'react-motion/lib/Motion';
+import spring from 'react-motion/lib/spring';
+
+const Collapsable = ({ fullHeight, minHeight, isVisible, children }) => (
+  <Motion defaultStyle={{height: isVisible ? fullHeight : minHeight }} style={{height: spring(!isVisible ? minHeight : fullHeight) }}>
+    {({ height }) =>
+      <div style={{ height: `${height}px`, overflow: 'hidden'}}>
+        {children}
+      </div>
+    }
+  </Motion>
+);
+
+Collapsable.propTypes = {
+  fullHeight: PropTypes.number.isRequired,
+  minHeight: PropTypes.number.isRequired,
+  isVisible: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 const messages = defineMessages({
   welcome: { id: 'welcome.message', defaultMessage: 'Welcome to {domain}!' },
@@ -28,6 +47,7 @@ class Announcements extends React.PureComponent {
 
   state = {
     show: true,
+    first: true,
   };
 
   onClick = () => {
@@ -45,24 +65,27 @@ class Announcements extends React.PureComponent {
 
   render () {
     const { intl, isEmptyHome } = this.props;
-    const { show } = this.state;
 
-    if (!isEmptyHome) {
-      return null;
+    if(this.state.first && !isEmptyHome) {
+      this.state.first = false;
+      this.state.show = false;
     }
+
     return (
       <ul className='announcements'>
-        <li style={{display: show ? '' : 'none'}}>
-          <div className='announcements__body'>
-            <p>{this.nl2br(intl.formatMessage(messages.welcome, {domain: document.title}))}</p>
-            {hashtags.map(hashtag =>
-              <Link to={`/timelines/tag/${hashtag}`}>
-                #{hashtag}
-              </Link>
-            )}
-          </div>
+        <li>
+          <Collapsable isVisible={this.state.show} fullHeight={300} minHeight={20} >
+            <div className='announcements__body'>
+              <p>{this.nl2br(intl.formatMessage(messages.welcome, {domain: document.title}))}</p>
+              {hashtags.map(hashtag =>
+                <Link to={`/timelines/tag/${hashtag}`}>
+                  #{hashtag}
+                </Link>
+              )}
+            </div>
+          </Collapsable>
           <div className='announcements__icon'>
-            <IconButton icon='times' onClick={this.onClick} size={16} />
+            <IconButton icon='caret-up' onClick={this.onClick} size={20} animate={true} active={this.state.show} />
           </div>
         </li>
       </ul>
