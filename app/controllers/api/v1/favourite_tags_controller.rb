4480de015b54531b67d2cd81de49d89fbb3b4206
fvh_P <ko_kurihara@yahoo.co.jp>
2017-08-22 01:17:01 +0000

    add POST and DELETE method on api/v1/favourite_tags

--- app/controllers/api/v1/favourite_tags_controller.rb
+++ app/controllers/api/v1/favourite_tags_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Api::V1::FavouriteTagsController < Api::BaseController
-  before_action -> { doorkeeper_authorize! :read }
+
+  before_action :set_account
+  before_action -> { doorkeeper_authorize! :read }, only: [:index]
+  before_action -> { doorkeeper_authorize! :write }, except: [:index]
   before_action :require_user!
 
   respond_to :json
@@ -9,5 +12,29 @@ class Api::V1::FavouriteTagsController < Api::BaseController
   def index
     @favourite_tags = current_account.favourite_tags.includes(:tag).map(&:to_json_for_api)
     render json: @favourite_tags
+  end
+  
+  def create
+    tag = find_tag
+    @favourite_tag = FavouriteTag.new(account: @account, tag: tag)
+    @favourite_tag.save
+    index
+  end
+  
+  def destroy
+    tag = find_tag
+    @favourite_tag = @account.favourite_tags.find_by(tag: tag)
+    @favourite_tag.destroy unless @favourite_tag.nil?
+    index
+  end
+
+  private
+
+  def set_account
+    @account = current_user.account
+  end
+
+  def find_tag
+    Tag.find_or_initialize_by(name: params[:tag])
   end
 end
