9e7beb04e437ee463ca25a7bb9f59b99561bdea0
fvh_P <ko_kurihara@yahoo.co.jp>
2017-08-26 13:37:24 +0000

    add visibility setting

--- app/controllers/api/v1/favourite_tags_controller.rb
+++ app/controllers/api/v1/favourite_tags_controller.rb
@@ -13,14 +13,16 @@ class Api::V1::FavouriteTagsController < Api::BaseController
     @favourite_tags = current_account.favourite_tags.includes(:tag).map(&:to_json_for_api)
     render json: @favourite_tags
   end
-  
+
   def create
     tag = find_tag
-    @favourite_tag = FavouriteTag.new(account: @account, tag: tag)
+    @favourite_tag = @account.favourite_tags.where(tag: tag).where.not(visibility: favourite_tag_visibility).first
+    @favourite_tag.destroy unless @favourite_tag.nil?
+    @favourite_tag = FavouriteTag.new(account: @account, tag: tag, visibility: favourite_tag_visibility)
     @favourite_tag.save
     index
   end
-  
+
   def destroy
     tag = find_tag
     @favourite_tag = @account.favourite_tags.find_by(tag: tag)
@@ -30,11 +32,19 @@ class Api::V1::FavouriteTagsController < Api::BaseController
 
   private
 
+  def tag_params
+    params.permit(:tag, :visibility)
+  end
+
   def set_account
     @account = current_user.account
   end
 
   def find_tag
-    Tag.find_or_initialize_by(name: params[:tag])
+    Tag.find_or_initialize_by(name: tag_params[:tag])
+  end
+
+  def favourite_tag_visibility
+    tag_params[:visibility].nil? ? 'public' : tag_params[:visibility]
   end
 end
