af242a4eb63f817bed349cd2f143b27d991d94c0
lnanase <nanase.ao@gmail.com>
2017-11-30 22:16:48 +0900

    fix migration date

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 # == Schema Information
 #
-# Table name: Announcements
+# Table name: announcements
 #
 #  id         :integer          not null, primary key
-#  title      :string           not null
-#  body       :text             not null
+#  title      :string           default(""), not null
+#  body       :text             default(""), not null
 #  link       :json
 #  disabled   :boolean          default(FALSE), not null
 #  created_at :datetime         not null
