316de20ee9f65dbaa425d2de97c2d2dbd3c32a7e
takayamaki <takayamaki@takayamaki.jp>
2020-06-01 02:15:19 +0900

    本家での実装に伴い不要になった独自お知らせ機能のテーブルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -196,22 +196,6 @@ ActiveRecord::Schema.define(version: 2019_10_01_213028) do
     t.index ["target_type", "target_id"], name: "index_admin_action_logs_on_target_type_and_target_id"
   end
 
-  create_table "announcement_links", force: :cascade do |t|
-    t.bigint "announcement_id", null: false
-    t.string "text", default: "", null: false
-    t.string "url", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["announcement_id"], name: "index_announcement_links_on_announcement_id"
-  end
-
-  create_table "announcements", force: :cascade do |t|
-    t.string "body", default: "", null: false
-    t.integer "order", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "backups", force: :cascade do |t|
     t.bigint "user_id"
     t.string "dump_file_name"
@@ -706,6 +690,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_213028) do
     t.bigint "poll_id"
     t.datetime "deleted_at"
     t.index ["account_id", "id", "visibility", "updated_at"], name: "index_statuses_20190820", order: { id: :desc }, where: "(deleted_at IS NULL)"
+    t.index ["created_at", "local", "id"], name: "index_statuses_on_created_at_and_local_and_id"
     t.index ["id", "account_id"], name: "index_statuses_local_20190824", order: { id: :desc }, where: "((local OR (uri IS NULL)) AND (deleted_at IS NULL) AND (visibility = 0) AND (reblog_of_id IS NULL) AND ((NOT reply) OR (in_reply_to_account_id = account_id)))"
     t.index ["in_reply_to_account_id"], name: "index_statuses_on_in_reply_to_account_id"
     t.index ["in_reply_to_id"], name: "index_statuses_on_in_reply_to_id"
@@ -835,7 +820,6 @@ ActiveRecord::Schema.define(version: 2019_10_01_213028) do
   add_foreign_key "account_warnings", "accounts", on_delete: :nullify
   add_foreign_key "accounts", "accounts", column: "moved_to_account_id", on_delete: :nullify
   add_foreign_key "admin_action_logs", "accounts", on_delete: :cascade
-  add_foreign_key "announcement_links", "announcements", on_delete: :cascade
   add_foreign_key "backups", "users", on_delete: :nullify
   add_foreign_key "blocks", "accounts", column: "target_account_id", name: "fk_9571bfabc1", on_delete: :cascade
   add_foreign_key "blocks", "accounts", name: "fk_4269e03e65", on_delete: :cascade
